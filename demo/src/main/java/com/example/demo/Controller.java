a10b574e5ca07ffec3a947798fbaa24262b6e31e
Jaime Jimenez <jaimejimenezrios10@gmail.com>
2021-09-20 11:46:36 +0200

    first commit

---
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping({"/", "/index"})
+    public String welcome(){
+
+
+
+
+    }
+
+}
+
+
+
